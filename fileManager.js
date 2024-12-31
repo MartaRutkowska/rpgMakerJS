698e2fa3edcf854b121c739175c2eded7042eeef
marta_rutkowska <marta.rutkowska.cs@gmail.com>
2024-12-31 15:49:56 +0100

    loading and saving gamefile, serialization/deserialization

--- fileManager.js
+++ fileManager.js
@@ -1,13 +1,33 @@
 export class fileManager {
 
     saveFile(data) {
-
-        const file = new Blob([data], {type: 'text/plain'});
+        let serialized = JSON.stringify(data);
+        const file = new Blob([serialized], {type: 'text/plain'});
 
         const link = document.createElement('a');
         link.href = URL.createObjectURL(file);
         link.download = "game.txt";
         link.click();
         URL.revokeObjectURL(link.href);
+    }
+
+    loadFile()
+    {
+        let file = document.getElementById("loadFile").files[0];
+        if(file)
+        {
+            let reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+
+            return new Promise((resolve, reject) => {
+
+                reader.onload = function (ev) {
+                    resolve(ev.target);
+                };
+                reader.onerror = () => {
+                    reject(console.log("file upload error"));
+                }
+            });
+        }
     }
 }
--- index.js
+++ index.js
@@ -2,29 +2,50 @@ import { fileManager } from './fileManager.js';
 import { storyBlock } from './storyBlock.js';
 import { Game } from './Game.js'
 
-var fileHelp = new fileManager();
-
 document.getElementById("saveFile").addEventListener('click', saveFile);
+document.getElementById("loadFile").addEventListener('change', loadFile);
 document.getElementById("startGame").addEventListener('click', startGame);
+
+var fileHelp = new fileManager();
+var gameInstance = undefined;
 
 function saveFile()
 {
-    fileHelp.saveFile('i found it');
+    fileHelp.saveFile(gameInstance);
+}
+
+function loadFile()
+{
+    var loadFilePromise = fileHelp.loadFile();
+    loadFilePromise.then((file) => {
+        try{
+            let deserializedGame = JSON.parse(file.result);
+            gameInstance = new Game(deserializedGame.storyBlocks);
+        }
+        catch{
+            alert("faulty game file");
+            return;
+        }
+    })
 }
 
 function startGame() {
 
-    let startBlock = new storyBlock(1,"Choose a door to go through",
-        [ {choice: "Red door", nextId: 2 }, {choice: "Green door", nextId: 2}]);
+    if(gameInstance === undefined)
+    {
+        let startBlock = new storyBlock(1,"Choose a door to go through",
+            [ {choice: "Red door", nextId: 2 }, {choice: "Green door", nextId: 2}]);
 
-    let block = new storyBlock(2, "U see a giant troll.",
-        [ {choice: "Run", nextId: 3 }, {choice: "Fight", nextId: 3}]);
+        let block = new storyBlock(2, "U see a giant troll.",
+            [ {choice: "Run", nextId: 3 }, {choice: "Fight", nextId: 3}]);
 
-    let end = new storyBlock(3, "Troll killed u either way. The End.",
-        []);
+        let end = new storyBlock(3, "Troll killed u either way. The End.",
+            []);
 
-    let game = new Game([startBlock, block, end]);
-    game.prepareGame();
-    game.startGame();
+        gameInstance = new Game([startBlock, block, end]);
+    }
+
+    gameInstance.prepareGame();
+    gameInstance.startGame();
 }
 
